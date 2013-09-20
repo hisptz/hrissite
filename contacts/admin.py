6f61a0378b80f78491ecdeddd9334484a437e6e5
chingalo <profschingalo@gmail.com>
2013-09-20 15:09:01 +0300

    resolve return in contacts app models and customize admin of contact app

--- contacts/admin.py
+++ contacts/admin.py
@@ -1,4 +1,13 @@
 from django.contrib import admin
 from contacts.models import *
-
-admin.site.register(Contacts)
+class MembersAdmin(admin.ModelAdmin):
+	fieldsets=[
+	('Name', {'fields':['member_name']}),
+	('Position in project', {'fields':['position']}),
+	('Institution / company', {'fields':['address']}),
+	('E-mail', {'fields':['e_mail']}),
+	('Phone Number', {'fields':['phone_number']}),	
+	]
+	list_display=('member_name','position','address','e_mail','phone_number',)
+	search_fields = ['member_name']
+admin.site.register(Contacts,MembersAdmin)
--- contacts/models.py
+++ contacts/models.py
@@ -5,12 +5,12 @@ from django.db import models
 class Contacts(models.Model):
 	member_name=models.CharField(max_length=200)
 	position=models.CharField(max_length=200)
-	phone_number=models.CharField(max_length=15, default='+255')
+	phone_number=models.CharField(max_length=15, null=True)
 	e_mail=models.CharField(max_length=200)
 	address=models.CharField(max_length=200)
 	
 	def __unicode__(self):   # Python 3: def __str__(self):
-		return self.name
+		return self.member_name
 			
 class ContactsPlugin(CMSPlugin):
 	title=models.CharField(max_length=200, default='contacts')
